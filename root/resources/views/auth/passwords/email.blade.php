fb9e81cf703020a1141142d9a72b456a8e87ef76
Victor Dario Vera <VDVera@proveedor.teco.com.ar>
2020-09-25 12:03:56 -0300

    add style login reset email

--- root/resources/views/auth/passwords/email.blade.php
+++ root/resources/views/auth/passwords/email.blade.php
@@ -11,7 +11,7 @@
 
             <div class="card-body">
                 @if (session('status'))
-                    <div class="alert alert-success" role="alert">
+                    <div class="login-form-alert" role="alert">
                         {{ session('status') }}
                     </div>
                 @endif
