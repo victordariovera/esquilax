39c38437b59fff600ea373d4fdb993a46404f7f3
Victor Dario Vera <VDVera@proveedor.teco.com.ar>
2020-10-15 10:28:05 -0300

    Fix layours for login page

--- root/resources/views/home.blade.php
+++ root/resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.default')
 @section('content')
     <h1>Bienvenidos a esquilax</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat laboriosam suscipit magni, debitis aliquid alias sit minima iure saepe perspiciatis repudiandae labore quas voluptas iste beatae, neque at blanditiis nobis. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt saepe fugit laborum dolores ipsa? Aperiam voluptatibus laborum voluptates, molestiae eius distinctio labore! Optio, aperiam animi. Quia accusantium nihil voluptatibus eveniet!</p>
