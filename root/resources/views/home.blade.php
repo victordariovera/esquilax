c29904834f970748e8df622b024b129524adb60d
Victor Dario Vera <VDVera@proveedor.teco.com.ar>
2020-10-15 10:23:55 -0300

    Content: Add Style to h1 and p

--- root/resources/views/home.blade.php
+++ root/resources/views/home.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 @section('content')
+    <h1>Bienvenidos a esquilax</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat laboriosam suscipit magni, debitis aliquid alias sit minima iure saepe perspiciatis repudiandae labore quas voluptas iste beatae, neque at blanditiis nobis. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt saepe fugit laborum dolores ipsa? Aperiam voluptatibus laborum voluptates, molestiae eius distinctio labore! Optio, aperiam animi. Quia accusantium nihil voluptatibus eveniet!</p>
 
 
 @endsection
