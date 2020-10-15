a960fb858bc0e9495d695b6f648135ca52cfc8f7
Victor Dario Vera <VDVera@proveedor.teco.com.ar>
2020-10-15 09:53:18 -0300

    Change color navigation bar.

--- root/resources/views/home.blade.php
+++ root/resources/views/home.blade.php
@@ -9,11 +9,12 @@
     </div>
     <div class="nav_items">
     <ul>
-      <li><img class="nav-item" alt="Guardias" title="Guardias" src="./img/phone-line.svg"></li>
-      <li><img class="nav-item" alt="Integraciones" title="Integraciones" src="./img/cloud-line.svg"></li>
-      <li><img class="nav-item" alt="Remediaciones" title="Remediaciones" src="./img/medicine-bottle-line.svg"></li>
-      <li><img class="nav-item" alt="Ajustes" title="Ajustes" src="./img/settings-3-line.svg"></li>
-      <li><a  href="{{ route('logout') }}" onclick="event.preventDefault();
+      <li title="Guardias"><img class="nav-item" alt="Guardias" title="Guardias" src="./img/phone-line.svg"></li>
+      <li title="Integraciones"><img class="nav-item" alt="Integraciones" title="Integraciones" src="./img/cloud-line.svg"></li>
+      <li title="Remediaciones"><img class="nav-item" alt="Remediaciones" title="Remediaciones" src="./img/medicine-bottle-line.svg"></li>
+      <li title="Ajustes"><img class="nav-item" alt="Ajustes" title="Ajustes" src="./img/settings-3-line.svg"></li>
+      <li title="Salir de Esquilax" onclick="event.preventDefault();
+        document.getElementById('logout-form').submit();"><a  href="{{ route('logout') }}" onclick="event.preventDefault();
         document.getElementById('logout-form').submit();"><img class="nav-vertical-link-icon" alt="Salir de Esquilax" title="Salir de Esquilax" src="./img/logout-box-line.svg"></a></li>
     </ul>
     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
