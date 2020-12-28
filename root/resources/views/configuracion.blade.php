8597ef8055295c3b225e935e4e40d662402ae9e3
Victor Vera <victordariovera@gmail.com>
2020-12-28 09:42:08 -0300

    fix img tag for todo in configuration section.

--- root/resources/views/configuracion.blade.php
+++ root/resources/views/configuracion.blade.php
@@ -7,7 +7,7 @@
 
     <div class="content-empty-container">
         <div class="content-empty">
-        <img scr="{{ asset('img/camping.svg') }}"><br> estamos creando algo genial, pronto estarà listo!
+        <img src="{{ asset('img/camping.svg') }}"><br> estamos creando algo genial, pronto estarà listo!
         </div>
     </div>
 
